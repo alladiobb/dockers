b94e7652a39227c70748e753588f681568527c97
Alladio Bonesso <Alladio@users.noreply.github.com>
2023-04-25 22:26:31 -0300

    Create docker-01.sh

---
+++ docker-01.sh
@@ -0,0 +1,10 @@
+#lista os arquivos
+docker ps
+#como se fizesse hellow-world:latest (Pega a ultima versão)
+docker run hellow-world
+
+#se ele executar e não aparecer quando você digitar comando:
+docker ps
+#tente
+docker ps -a
+#ele irá mostrar o histórico
