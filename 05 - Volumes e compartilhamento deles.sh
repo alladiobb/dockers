0c12d1ce5bd908267d3d2433a86b944ed2b85f18
Alladio Bonesso <Alladio@users.noreply.github.com>
2023-04-30 01:47:45 -0300

    volumes e compartilhamentos

---
+++ 05 - Volumes e compartilhamento deles.sh
@@ -0,0 +1,30 @@
+#verificar os commandos de volumes
+docker volume COMMAND
+
+#criando volume
+docker volume create novovolume
+
+#listando volumes
+docker volume ls
+
+#
+docker volume inspect
+
+#exemplo - Criando pasta "app"
+docker run --name nginx -d --mount type=volume, source=meuvolume, target=/app nginx
+
+#para verificar a pasta
+docker exec -it nginx bash
+ls
+cd app
+
+#criando um arquivo assim ao rodar outros dockers 
+#eles compartilharão as mesmas pastas e arquivos
+docker run --name nginx2 -d --mount type=volume, source=meuvolume, target=/app nginx
+#OU usando -v
+docker run --name nginx3 -d -v meuvolume:/app nginx
+
+#Cuidados com os arquivos e diretórios de volumes
+#matar e limpar os volumes
+docker volume prume
+
