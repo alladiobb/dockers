4e8c208bea6893a2fe7acc3ee391f4e00c9d26d8
Alladio Bonesso <Alladio@users.noreply.github.com>
2023-04-30 01:28:18 -0300

    atualizando nomes dos arquivos e atualizando uso de binds

--- 04 - Usando pasta do sistema no Docker.sh
+++ 04 - Usando pasta do sistema no Docker.sh
@@ -4,8 +4,15 @@ docker run -d --name nginx -p 8080:80 -v /Users/alladiobonesso/Documents/Projeto
 #comando mais explicito para roda a pasta
 docker run -d --name nginx -p 8080:80 --mount type=bind,source="$(pwd)"/html,target=/usr/share/nginx/html nginx
 
+
 #diferença de uma para o outro
 #cria a pasta
 -v
-#dará erro se a pasta não existir
+#dará erro se a pasta não existir, pois o MOUNT não cria pastas
 --mount
+
+#sem mapear porta
+docker run -d -v "$(pwd)"/html/x:/user/share/html nginx
+
+#usando mount
+docker run -d --mount type=bind, source="$(pwd)"/html/x, target=/user/share/html nginx
