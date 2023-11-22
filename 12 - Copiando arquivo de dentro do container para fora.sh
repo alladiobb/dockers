fe25ef8abaef7ff8802d24e74ce52adb67b707e9
Alladio Bonesso <Alladio@users.noreply.github.com>
2023-11-22 12:51:43 -0300

    arrumando o código para melhor entendimento

--- 12 - Copiando arquivo de dentro do container para fora.sh
+++ 12 - Copiando arquivo de dentro do container para fora.sh
@@ -1,5 +1,6 @@
 #comando para copiar arquivos gerados de dentro do container para o host
 docker cp [ID_DO_CONTAINER]:[CAMINHO_NO_CONTÊINER] [CAMINHO_NO_HOST]
-
 docker cp 86167667fd75:/app/output.parquet .
+
+#resultado esperado
 Successfully copied 160kB to /home/alladio/github/dataengineer-transformations-python/resources/citibike/.
