abae7a825fd24564044b78f3bafa1ab3dabb0c9e
Alladio Bonesso <Alladio@users.noreply.github.com>
2023-11-24 09:04:32 -0300

    Gerando a imagem da aplicação Node.js

--- 13 - Dockerfile envoironment.sh
+++ 13 - Dockerfile envoironment.sh
@@ -4,4 +4,10 @@
 
 #criando container docker com node para usar ele como ambiente de dev
 
-docker --rm -it -v $(pwd)/:/usr/src/app -p 3000:3000 node:latest bash
+docker run --rm -it -v $(pwd)/:/usr/src/app -p 3000:3000 node:latest bash7
+
+#faz toda instalação do node necessaria
+apt-get update
+npm install express --save
+npm init
+#edita o HTML -> html.js
