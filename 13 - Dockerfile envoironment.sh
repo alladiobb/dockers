54ec850f20c733cd986f76d89ab5f8087ab86df2
Alladio Bonesso <Alladio@users.noreply.github.com>
2023-11-24 10:09:33 -0300

    add content

--- 13 - Dockerfile envoironment.sh
+++ 13 - Dockerfile envoironment.sh
@@ -4,11 +4,19 @@
 
 #criando container docker com node para usar ele como ambiente de dev
 
-docker run --rm -it -v $(pwd)/:/usr/src/app -p 3000:3000 node:latest bash
+docker run --rm -it -v $(pwd)/:/usr/src/app -p 3000:3000 node:latest bash7
 
 #faz toda instalação do node necessaria
 apt-get update
-npm init
 npm install express --save
-#edita o HTML -> html.js
+npm init
+#edita o HTML -> index.js
 #atentar arquivo na pasta /node no projeto
+
+node index.js
+
+#verifica se funcinou -> localhost:3000
+
+#AGORA SAIMOS DA IMAGEM E RODAMOS CRIAMOS O DOCKERFILE: que está dentro da pasta node/Dockerfile
+
+docker build -t nodex .
