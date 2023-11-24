e98a66863b7879296031d16c2c47a13e67f24c96
Alladio Bonesso <Alladio@users.noreply.github.com>
2023-11-24 10:01:13 -0300

    fix 13 and rename index.html

--- 13 - Dockerfile envoironment.sh
+++ 13 - Dockerfile envoironment.sh
@@ -4,11 +4,11 @@
 
 #criando container docker com node para usar ele como ambiente de dev
 
-docker run --rm -it -v $(pwd)/:/usr/src/app -p 3000:3000 node:latest bash7
+docker run --rm -it -v $(pwd)/:/usr/src/app -p 3000:3000 node:latest bash
 
 #faz toda instalação do node necessaria
 apt-get update
-npm install express --save
 npm init
+npm install express --save
 #edita o HTML -> html.js
 #atentar arquivo na pasta /node no projeto
