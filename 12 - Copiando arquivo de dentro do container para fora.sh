98173a0c301b4108242e935f3a0cf52882c26da6
Alladio Bonesso <Alladio@users.noreply.github.com>
2023-11-24 11:25:36 -0300

    share volume container and host

--- 12 - Copiando arquivo de dentro do container para fora.sh
+++ 12 - Copiando arquivo de dentro do container para fora.sh
@@ -4,3 +4,8 @@ docker cp 86167667fd75:/app/output.parquet .
 
 #resultado esperado
 Successfully copied 160kB to /home/alladio/github/dataengineer-transformations-python/resources/citibike/.
+
+#OU OU OU
+# -v -> compartilha o volume: [ diretório do HOST]:[diretório do container]
+docker run -v $(pwd)/:/usr/src/app
+
