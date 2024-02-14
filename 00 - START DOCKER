e841630522b970765c8e165976ded3cd3757ccde
Alladio Bonesso <13860253+alladiobb@users.noreply.github.com>
2024-02-14 10:23:55 -0300

    Refact: File name and add documentation link

--- 00 - START DOCKER
+++
@@ -1,1 +0,0 @@
-sudo service docker start
---
+++ 00 - START DOCKER.sh
@@ -0,0 +1,7 @@
+#Para instalar docker eu sempre recomendo a documentação oficial que está bem completa:
+
+# https://docs.docker.com/engine/install/ubuntu/
+
+
+#Para iniciar o serviço caso o mesmo não inicie automaticamente:
+sudo service docker start
